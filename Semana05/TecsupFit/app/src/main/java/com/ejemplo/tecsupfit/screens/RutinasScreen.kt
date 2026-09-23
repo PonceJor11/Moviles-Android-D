8f1e283f79611eab9ee37e90cff3ceecffd12415
Jordy <jordyponce@tecsup.edu.pe>
2026-09-23 10:59:25 -0500

    feat(ui): implemento pantallas de Confirmacion, Mis Reservas, Rutinas y Perfil con tipografia personalizada

---
+++ Semana05/TecsupFit/app/src/main/java/com/ejemplo/tecsupfit/screens/RutinasScreen.kt
@@ -0,0 +1,17 @@
+package com.ejemplo.tecsupfit.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RutinasScreen() {
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text(text = "Rutinas de entrenamiento", fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Próximamente disponible.")
+    }
+}
